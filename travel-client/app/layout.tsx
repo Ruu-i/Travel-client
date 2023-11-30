215d33b2210e6e04cb194d9829cf626ec49f9b22
Ruwani <ruwani054@gmail.com>
2023-12-01 00:08:36 +0530

    project setup done and navbar partially done

---
+++ travel-client/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { Nunito } from 'next/font/google'
+import Navbar from './components/Navbar/Navbar';
+
+const font = Nunito({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Travel-pal',
+  description: 'app like airbnb',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Navbar />
+        {children}</body>
+    </html>
+  )
+}
---
+++ travel-client/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div className='text-rose-500 text-2xl'>hello</div>
+   
+  )
+}
